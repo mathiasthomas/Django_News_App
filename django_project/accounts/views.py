ead70554323c3e62df6acafd313721da0f6fab49
mathiasthomas <mathiasthomas17@gmail.com>
2023-07-23 06:59:02 +0300

    Authentication
    Added Login and Sign Up Capabilities

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import UserCreationForm
+from django.views.generic import CreateView
 
-# Create your views here.
+
+# ----------Signup View ---------
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
