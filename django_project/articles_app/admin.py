7e613bfb9c36bcaa652286579196fd9c9368bc8b
mathiasthomas <mathiasthomas17@gmail.com>
2023-07-29 08:51:49 +0300

    Comment System
    Started Working On Comment System

--- django_project/articles_app/admin.py
+++ django_project/articles_app/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+# 1 < ------///  StackedInline view ------ ///>
+# class CommentInline(admin.StackedInline):
+#     model = Comment
+#     extra = 5  # By Default, the fields are 3, but you can add Extras
+#
+#
+# class ArticleAdmin(admin.ModelAdmin):
+#     inlines = [
+#         CommentInline
+#     ]
+
+# 2 < ------///  TabularInline view ------ ///> Preferred
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline
+    ]
+
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
